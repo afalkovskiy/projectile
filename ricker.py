3df3e6d31897a66e1bcd3425ef528021a33ef810
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-23 23:25:15 -0600

    Update ricker.py

--- ricker.py
+++ ricker.py
@@ -13,6 +13,19 @@ def ricker(f, length=0.512, dt=0.001):
     y = (1.-2.*(np.pi**2)*(f**2)*(t**2))*np.exp(-(np.pi**2)*(f**2)*(t**2))
     return t, y
 
+def ORMSBY(f1=5., f2=10., f3=40., f4=45., length=0.512, dt=0.001):
+    p = np.pi
+    t = np.linspace(-length/2, (length-dt)/2, length/dt)
+
+    # y = p*p*f4**2 * (np.sinc(f4*t))**2/(p*f4-p*f3) - p*p*f3**2 * (np.sinc(f3*t))**2/(p*f4-p*f3) - \
+    #     p*p*f2**2 * (np.sinc(f2*t))**2/(p*f2-p*f1) - p*p*f1**2 * (np.sinc(f1*t))**2/(p*f2-p*f1)
+    y = p*f4**2 * (np.sinc(f4*t))**2/(f4-f3) - p*f3**2 * (np.sinc(f3*t))**2/(f4-f3) - \
+        p*f2**2 * (np.sinc(f2*t))**2/(f2-f1) - p*f1**2 * (np.sinc(f1*t))**2/(f2-f1)
+
+    y = y / np.amax(abs(y))
+
+    return t, y
+
 #fig, ax = plt.subplots()
 f = 25
 
@@ -22,6 +35,7 @@ st.subheader("f(t) = (1.-2.*(np.pi**2)*(f**2)*(t**2))*np.exp(-(np.pi**2)*(f**2)*
 f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
 st.write("Frequency = ", f)
 t, y = ricker (f)
+t, y = ORMSBY(,,,f,200,,)
 
 chart_data = pd.DataFrame(
    {
