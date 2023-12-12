e437e85ab4e7eb4b6e9d5e6d6e9cae9a0cb8183e
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-12 00:14:10 -0700

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -23,15 +23,14 @@ def projectile(v, alpha, dt=0.01):
 st.title('Projectile') 
 #st.button('Hit me')
 
-# f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
-# st.write("Frequency = ", f)
-
-v = st.slider('Velocity (m/s)', value=120., min_value=1., max_value=1200.)
-st.write("Velocity = ", v)
-
-alpha = st.slider('Inclination to the horizontal (deg)', value=30., min_value=0., max_value=90.)
-alpha_rad = pi * alpha / 180.
-st.write("alpha_rad = ", alpha_rad)
+col1, col2 = st.columns(2)
+with col1:
+    v = st.slider('Velocity (m/s)', value=120., min_value=1., max_value=1200.)
+    st.write("Velocity = ", v)
+with col2:
+    alpha = st.slider('Inclination to the horizontal (deg)', value=30., min_value=0., max_value=90.)
+    alpha_rad = pi * alpha / 180.
+    st.write("alpha_rad = ", alpha_rad)
 
 st.subheader("Vx = V * Cos(alpha) = ")
 
