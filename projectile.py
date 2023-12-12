2ac5e28ba4578cf0fa70fda2e55ff6bcdd21ebf2
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-12 13:59:07 -0700

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -73,7 +73,7 @@ plt.grid(True)
 st.pyplot(fig)
 
 st.latex(r'''
-v_x = v cos( \times \theta ), v_y = v sin( \times \theta )
+v_x = v \cdot cos( \theta ); v_y = v \cdot sin( \theta )
 ''') 
 
 st.latex(r'''
