7b687a48c6db21388e2904e3e2110ed8054dc0de
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-12-11 23:52:40 -0700

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -18,32 +18,13 @@ def projectile(v, alpha, dt=0.01):
     return x, y
     
 
-def ricker(f, length=0.512, dt=0.001):
-    t = np.linspace(-length/2, (length-dt)/2, int(length/dt))
-    y = (1.-2.*(np.pi**2)*(f**2)*(t**2))*np.exp(-(np.pi**2)*(f**2)*(t**2))
-    return t, y
-
-def ORMSBY(f1=5., f2=10., f3=40., f4=45., length=0.512, dt=0.001):
-    p = np.pi
-    t = np.linspace(-length/2, (length-dt)/2, int(length/dt))
-
-    # y = p*p*f4**2 * (np.sinc(f4*t))**2/(p*f4-p*f3) - p*p*f3**2 * (np.sinc(f3*t))**2/(p*f4-p*f3) - \
-    #     p*p*f2**2 * (np.sinc(f2*t))**2/(p*f2-p*f1) - p*p*f1**2 * (np.sinc(f1*t))**2/(p*f2-p*f1)
-    y = p*f4**2 * (np.sinc(f4*t))**2/(f4-f3) - p*f3**2 * (np.sinc(f3*t))**2/(f4-f3) - \
-        p*f2**2 * (np.sinc(f2*t))**2/(f2-f1) - p*f1**2 * (np.sinc(f1*t))**2/(f2-f1)
-
-    y = y / np.amax(abs(y))
-
-    return t, y
-
 #fig, ax = plt.subplots()
-f = 25
 
 st.title('Projectile') 
 #st.button('Hit me')
-st.subheader("f(t) = (1.-2.*(np.pi**2)*(f**2)*(t**2))*np.exp(-(np.pi**2)*(f**2)*(t**2))")
-f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
-st.write("Frequency = ", f)
+
+# f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
+# st.write("Frequency = ", f)
 
 v = st.slider('Velocity (m/s)', value=120., min_value=1., max_value=1200.)
 st.write("Velocity = ", v)
@@ -52,13 +33,7 @@ alpha = st.slider('Inclination to the horizontal (deg)', value=30., min_value=0.
 alpha_rad = pi * alpha / 180.
 st.write("alpha_rad = ", alpha_rad)
 
-
-# t, y = ricker (f)
-# f1 = 5
-# f2 = 10
-# f3 = f
-# f4 = f+20.
-# t, y = ORMSBY(f1, f2, f3, f4, 0.512, 0.001)
+st.subheader("Vx = V * Cos(alpha) = ")
 
 t, y = projectile(v, alpha_rad)
 
